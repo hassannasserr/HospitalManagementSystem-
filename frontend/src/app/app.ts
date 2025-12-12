3e97251223c022113915dbaeb7b7743cfbce1609
Mahmoud Ayman <2301115@student.eelu.edu.eg>
2025-12-12 20:29:08 +0200

    landing page init

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Register } from './features/auth/components/register/register';
+import { Home } from "./features/landing/pages/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Register],
+  imports: [RouterOutlet, Register, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
